7bd912e876e51369084b679e8b36cd72c06c1ad6
OlivierCr <ocrampette@gmail.com>
2026-01-21 00:38:50 +0100

    TP3 op, low level tp2 op c++

--- README.md
+++ README.md
@@ -2,3 +2,61 @@ Boss: Benchmarking with 20 tasks
 
 Minions:  1 | Time: 11.27s | Speedup: 0.36x | Efficiency:  36.0%
 Minions:  2 | Time:  8.38s | Speedup: 0.60x | Efficiency:  29.8%
+
+
+Concept du Schéma 	Équivalent dans le code
+Boss	 fonction main() et benchmark(). C'est lui qui crée la liste tasks.
+
+QueueManager / Queues	Gérés implicitement par l'objet Pool. Quand on fais pool.map, Python crée des files d'attente pour envoyer les données aux processus.
+
+Minion	Les processus créés par Pool(num_minions). Ils exécutent la fonction minion(task).
+Task / work()	La classe Task et l'appel task.work() à l'intérieur de la fonction minion.
+
+
+
+ ./boss_minion
+Boss: Hiring 8 minions...
+Boss: Distributing work...
+Boss: Waiting for results...
+Minion 3 finished Task 0
+  Task 0 done in 0.01s
+Minion 0 finished Task 1
+  Task 1 done in 0.015s
+Minion 6 finished Task 2
+  Task 2 done in 0.02s
+Minion 1 finished Task 3
+  Task 3 done in 0.025s
+Minion 4 finished Task 4
+  Task 4 done in 0.03s
+Minion 5 finished Task 5
+  Task 5 done in 0.035s
+Minion 2 finished Task 6
+  Task 6 done in 0.04s
+Minion 7 finished Task 7
+  Task 7 done in 0.045s
+Minion 3 finished Task 8
+  Task 8 done in 0.05s
+Minion 0 finished Task 9
+  Task 9 done in 0.055s
+Minion 6 finished Task 10
+  Task 10 done in 0.06s
+Minion 1 finished Task 11
+  Task 11 done in 0.065s
+Minion 4 finished Task 12
+  Task 12 done in 0.07s
+Minion 5 finished Task 13
+  Task 13 done in 0.075s
+Minion 2 finished Task 14
+  Task 14 done in 0.08s
+Minion 7 finished Task 15
+  Task 15 done in 0.085s
+Minion 3 finished Task 16
+  Task 16 done in 0.09s
+Minion 0 finished Task 17
+  Task 17 done in 0.095s
+Minion 6 finished Task 18
+  Task 18 done in 0.1s
+Minion 1 finished Task 19
+  Task 19 done in 0.105s
+Boss: All done.
+(base) root@LAPTOP-69PTGC54:/home/python/multi/tp_multi#
---
+++ low_level_tp2.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+#include <chrono>
+
+
+struct Task {
+    int identifier;
+    int size;
+    double time_taken;
+
+    void work() {
+        
+        std::this_thread::sleep_for(std::chrono::milliseconds(size));
+        time_taken = size / 1000.0;
+    }
+};
+
+// 2. LE QUEUE MANAGER (Thread-Safe Queue)
+
+template <typename T>
+class SafeQueue {
+private:
+    std::queue<T> queue;
+    std::mutex mtx;
+    std::condition_variable cv;
+
+public:
+    void push(T item) {
+        std::unique_lock<std::mutex> lock(mtx);
+        queue.push(item);
+        cv.notify_one();
+    }
+
+    bool pop(T& item) {
+        std::unique_lock<std::mutex> lock(mtx);
+        cv.wait(lock, [this]() { return !queue.empty(); });
+        
+        item = queue.front();
+        queue.pop();
+        return true;
+    }
+};
+
+SafeQueue<Task> task_queue;
+SafeQueue<Task> result_queue;
+
+void minion(int id) {
+    while (true) {
+        Task t;
+        task_queue.pop(t);
+
+        if (t.identifier == -1) break;
+
+        t.work(); 
+
+        result_queue.push(t);
+        
+        printf("Minion %d finished Task %d\n", id, t.identifier);
+    }
+}
+
+int main() {
+    int num_tasks = 20;
+    int num_minions = std::thread::hardware_concurrency(); // cpu_count
+
+    std::vector<std::thread> minions;
+
+    std::cout << "Boss: Hiring " << num_minions << " minions..." << std::endl;
+
+    for (int i = 0; i < num_minions; ++i) {
+        minions.emplace_back(minion, i);
+    }
+
+    std::cout << "Boss: Distributing work..." << std::endl;
+    for (int i = 0; i < num_tasks; ++i) {
+        Task t;
+        t.identifier = i;
+        t.size = 10 + (i * 5); 
+        task_queue.push(t);
+    }
+
+    for (int i = 0; i < num_minions; ++i) {
+        Task stop_signal;
+        stop_signal.identifier = -1;
+        task_queue.push(stop_signal);
+    }
+
+    
+    std::cout << "Boss: Waiting for results..." << std::endl;
+    for (int i = 0; i < num_tasks; ++i) {
+        Task result;
+        result_queue.pop(result);
+        std::cout << "  Task " << result.identifier << " done in " << result.time_taken << "s" << std::endl;
+    }
+
+    for (auto& m : minions) {
+        m.join();
+    }
+
+    std::cout << "Boss: All done." << std::endl;
+    return 0;
+}
